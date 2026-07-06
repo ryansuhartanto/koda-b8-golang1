e4a4768329da538859e51b97bab20e5864e64239
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-06 11:13:32 +0700

    feat: interactive input

--- main.go
+++ main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
+	"os"
+	"strconv"
 )
 
 func luas(
-	r float32,
-) float32 {
+	r float64,
+) float64 {
 	return math.Pi * r * r
 }
 
 func keliling(
-	r float32,
-) float32 {
+	r float64,
+) float64 {
 	return math.Pi * r * 2
 }
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("silahkan input jari-jari (r) lingkaran: ")
+	scanner.Scan()
+	r, err := strconv.ParseFloat(scanner.Text(), 32)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Kesalahan input!\n")
+		fmt.Fprintf(os.Stderr, "%s", err.Error())
+	}
+
 	fmt.Printf("lingkaran dengan r = 7 \n")
-	fmt.Printf("  luas    : %f\n", luas(7))
-	fmt.Printf("  keliling: %f\n", keliling(7))
+	fmt.Printf("  luas    : %f\n", luas(r))
+	fmt.Printf("  keliling: %f\n", keliling(r))
 }
